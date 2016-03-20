01a56fd4dff729bdc71c1485837861de8c02d748
Lu Yang @DESKTOP <young07ly@hotmail.com>
2016-03-20 17:42:13 +0800

    Refactored Logic

--- DODO/src/Logic/Logic.java
+++ DODO/src/Logic/Logic.java
@@ -20,8 +20,9 @@ public class Logic {
 	private ArrayList<Task> completedTasks;
 	private ArrayList<Task> overdueTasks;
 	private ArrayList<Task> floatingTasks;
+	private ArrayList<Category> categories;
 	private ArrayList<Task> results;
-	
+
 	/*************************************CONSTRUCTOR******************************************/
 	public static Logic getInstance(String directory) {
 		if (theOne==null) {
@@ -42,7 +43,7 @@ public class Logic {
 		storage.save(TASK_STATUS.OVERDUE, overdueTasks);
 		return "Saved successfully";
 	}
-	
+
 	/***********************************ACCESSORS***********************************************/
 	public ArrayList<Task> getOngoingTasks() {
 		return this.ongoingTasks;
@@ -59,8 +60,8 @@ public class Logic {
 	public ArrayList<Task> getOverdueTasks() {
 		return this.overdueTasks;
 	}
-	
-	
+
+
 	/***********************************PRIVATE METHODS***********************************************/
 	private Logic(String directory) {
 		System.out.println("[Logic/init] directory: " + directory);
@@ -78,53 +79,30 @@ public class Logic {
 
 		switch (command) {
 		case ADD:
-			return add(parser, data);
+			Add add = new Add(parser, data, COMMAND_TYPE.ADD);
+			return execute(add);
 		case DELETE:
-			return delete(parser, data);
+			Delete delete = new Delete(parser, data, COMMAND_TYPE.DELETE);
+			return execute(delete);
 		case EDIT:
-			return edit(parser, data);
+			Edit edit = new Edit(parser, data, COMMAND_TYPE.EDIT);
+			return execute(edit);
 		case COMPLETE:
-			return complete(parser, data);
-		case UNDO:
-			return undo();
+			Complete complete = new Complete(parser, data, COMMAND_TYPE.COMPLETE);
+			return execute(complete);
+		case TAG:
+			Tag tag = new Tag(parser, data, COMMAND_TYPE.TAG);
+			return execute(tag);
+		default:
+			return "Invalid COMMAND_TYPE.";
 		}
-		return "Invalid COMMAND_TYPE.";
-	}
-
-	private String add(Parser parser, ArrayList<ArrayList<Task>> data) {
-		Add add = new Add(parser, data, COMMAND_TYPE.ADD);
-		String status = add.execute();
-		data = add.getData();
-		update(data);
-		return status;
 	}
 	
-	private String delete(Parser parser, ArrayList<ArrayList<Task>> data) {
-		Delete delete = new Delete(parser, data, COMMAND_TYPE.DELETE);
-		String status = delete.execute();
-		data = delete.getData();
+	private String execute(Command command) {
+		String status = command.execute();
+		ArrayList<ArrayList<Task>> data = command.getData();
 		update(data);
 		return status;
-	}
-	
-	private String edit(Parser parser, ArrayList<ArrayList<Task>> data)  {
-		Edit edit = new Edit(parser, data, COMMAND_TYPE.EDIT);
-		String status = edit.execute();
-		data = edit.getData();
-		update(data);
-		return status;
-	}
-	
-	private String complete(Parser parser, ArrayList<ArrayList<Task>> data) {
-		Complete complete = new Complete(parser, data, COMMAND_TYPE.COMPLETE);
-		String status = complete.execute();
-		data = complete.getData();
-		update(data);
-		return status;
-	}
-
-	private String undo() {
-		return "Undo successful";
 	}
 
 	private ArrayList<ArrayList<Task>> compress() {
@@ -135,7 +113,7 @@ public class Logic {
 		tasks.add(overdueTasks);
 		return tasks;
 	}
-	
+
 	private String update(ArrayList<ArrayList<Task>> data) {
 		this.floatingTasks = data.get(0);
 		this.ongoingTasks = data.get(1);

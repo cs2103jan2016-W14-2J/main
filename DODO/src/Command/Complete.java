f6b6295041d518573ea34d9bfaa106d37722d7af
Lu Yang @DESKTOP <young07ly@hotmail.com>
2016-03-20 17:08:57 +0800

    Fixed several bugs of Complete
    1. Logic bug
    2. Catch arrayoutofbound

--- DODO/src/Command/Complete.java
+++ DODO/src/Command/Complete.java
@@ -18,23 +18,28 @@ public class Complete extends Command {
 		case FLOATING:
 			return completeTask(index, this.floatingTasks);
 		case ONGOING:
-			return completeTask(index, this.floatingTasks);
+			return completeTask(index, this.ongoingTasks);
 		case COMPLETED:
 			return "Your task have been completed.";
 		case OVERDUE:
-			return completeTask(index, this.floatingTasks);
+			return completeTask(index, this.overdueTasks);
 		default:
 			return "Invalid command.";
 		} 
 	}
 
 	private String completeTask(int index, ArrayList<Task> tasks) {
+		try {
 		Task task = tasks.get(index);
 		if (task.getComplete()) {
 			return "Task " + index + " has been completed before.";
 		}
 		task.complete(true);
-		return null;
+		return "Congratulation! Task " + (index+INDEX_ADJUSTMENT) + " is completed.";
+		}
+		catch (IndexOutOfBoundsException e) {
+			return "Task " + (index+INDEX_ADJUSTMENT) + " is absent.";
+		}
 	}
 
 	@Override

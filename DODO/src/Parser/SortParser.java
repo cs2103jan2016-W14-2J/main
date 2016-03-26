910a23e583c167f4e611b59327e22ab06064d058
Verbal Criminal <a0125552@u.nus.edu>
2016-03-26 17:15:07 +0800

    Implemented sort_type enum class

--- DODO/src/Parser/SortParser.java
+++ DODO/src/Parser/SortParser.java
@@ -1,5 +1,40 @@
 package Parser;
 
-public class SortParser {
+import Command.*;
 
+public class SortParser {
+	
+	private String userTask;
+	
+	public SortParser(String userTask) {
+		this.userTask = userTask;
+		executeSortParser(userTask);
+	}
+
+	private void executeSortParser(String userTask) {
+		userTask = removeBy(userTask);
+		
+		switch(determineSearchType(userTask)) {
+			
+		case BY_DATE:
+			processByDate(userTask);
+			break;
+		case BY_TASK:
+			break;
+		case BY_TAG:
+			processByTag(userTask);
+			break;
+		case INVALID:
+			setSearchType(SEARCH_TYPE.INVALID);
+			break;
+		}
+	}
+	
+	private String removeBy(String userTask) {
+		String[] str = userTask.toLowerCase().split("\\s+");
+		if (str[0].contains("by")) {
+			userTask.replace(" by ", "");
+		}
+		return userTask;
+	}
 }

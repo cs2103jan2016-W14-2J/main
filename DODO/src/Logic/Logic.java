4af059da18fa2145ec9a10538edcd6fd03b35868
Lu Yang @DESKTOP <young07ly@hotmail.com>
2016-04-09 11:51:41 +0800

    Fixed bug with LastModifiedIndex

--- DODO/src/Logic/Logic.java
+++ DODO/src/Logic/Logic.java
@@ -124,7 +124,7 @@ public class Logic {
 	}
 	
 	public int getLastModifiedIndex() {
-		return this.getLastModifiedIndex();
+		return this.lastModifiedIndex;
 	}
 
 	/***********************************PRIVATE METHODS***********************************************/

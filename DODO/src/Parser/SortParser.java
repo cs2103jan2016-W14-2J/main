e476f958ae9b20e75c0b6a059ac1f50bf046a09f
Verbal Criminal <a0125552@u.nus.edu>
2016-04-10 02:47:21 +0800

    Refactor SortParser class.

--- DODO/src/Parser/SortParser.java
+++ DODO/src/Parser/SortParser.java
@@ -1,27 +1,42 @@
-//@@author: Hao Jie
+//@@author: A0125552L
 package Parser;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import Command.*;
 
 public class SortParser {
-
+	
+	private static Logger logger;
 	private String ALPHABETICAL_ORDER = "abc";
 	private String NUMERICAL_ORDER = "123";
 	private String REVERSE_ALPHABETICAL_ORDER = "cba";
 	private String REVERSE_NUMERICAL_ORDER = "321";
 	private String DATE_ORDER = "date";
-	private String PREPOSITION_BY = " by ";
+	private String PREPOSITION_BY = "by";
+	private int POSITION_OF_INPUT  = 0;
 	
 	private String STRING_SPLITTER = "\\s+";
 	private String STRING_EMPTY = "";
+	private String[] stringSplit;
 	
-	
+	// Logging messages for methods
+	private final String LOGGER_MESSAGE_DETERMINE_SORT_TYPE = "SortParser Class: Processing user input in determineSortType method.";
+	private final String LOGGER_MESSAGE_IS_REVERSE_ALPHABETICAL = "SortParser Class: Processing user input in isSortByReverseAlphabetical method.";
+	private final String LOGGER_MESSAGE_IS_ALPHABETICAL = "SortParser Class: Processing user input in isSortByAlphabetical method.";
+	private final String LOGGER_MESSAGE_IS_BY_DATE = "SortParser Class: Processing user input in isSortByDate method.";
+	private final String LOGGER_MESSAGE_REMOVE_BY = "SortParser Class: Processing user input in removeBy method.";
+			
 	public SortParser() {
+		logger = Logger.getLogger("SortParser");
 	}
 	
 	protected CommandUtils determineSortType(CommandUtils commandUtil, String userTask) {
 		
 		assert (userTask != null);
+		logger.log(Level.INFO, LOGGER_MESSAGE_DETERMINE_SORT_TYPE);
+		
 		userTask = removeBy(userTask.toLowerCase());
 		
 		if (isSortByDate(userTask)) {
@@ -40,27 +55,51 @@ public class SortParser {
 	}
 
 	private boolean isSortByReverseAlphabetical(String userTask) {
-		String[] str = userTask.toLowerCase().split(STRING_SPLITTER);
-		return (str[0].trim().contains(REVERSE_ALPHABETICAL_ORDER) ||
-				str[0].trim().contains(REVERSE_NUMERICAL_ORDER)) ? true : false;
+		
+		assert(userTask != null);
+		logger.log(Level.INFO, LOGGER_MESSAGE_IS_REVERSE_ALPHABETICAL);
+		
+		stringSplit = userTask.toLowerCase().split(STRING_SPLITTER);
+		
+		return (stringSplit[POSITION_OF_INPUT].trim().contains(REVERSE_ALPHABETICAL_ORDER) ||
+				stringSplit[POSITION_OF_INPUT].trim().contains(REVERSE_NUMERICAL_ORDER)) ? true : false;
+	
 	}
 
 	private boolean isSortByAlphabetical(String userTask) {
-		String[] str = userTask.toLowerCase().split(STRING_SPLITTER);
-		return (str[0].trim().contains(ALPHABETICAL_ORDER) ||
-				str[0].trim().contains(NUMERICAL_ORDER)) ? true : false;
+		
+		assert(userTask != null);
+		logger.log(Level.INFO, LOGGER_MESSAGE_IS_ALPHABETICAL);
+		
+		stringSplit = userTask.toLowerCase().split(STRING_SPLITTER);
+		
+		return (stringSplit[POSITION_OF_INPUT].trim().contains(ALPHABETICAL_ORDER) ||
+				stringSplit[POSITION_OF_INPUT].trim().contains(NUMERICAL_ORDER)) ? true : false;
+	
 	}
 
 	private boolean isSortByDate(String userTask) {
-		String[] str = userTask.toLowerCase().split(STRING_SPLITTER);
-		return (str[0].trim().contains(DATE_ORDER)) ? true : false;
+		
+		assert(userTask != null);
+		logger.log(Level.INFO, LOGGER_MESSAGE_IS_BY_DATE);
+		
+		stringSplit = userTask.toLowerCase().split(STRING_SPLITTER);
+		
+		return (stringSplit[POSITION_OF_INPUT].trim().contains(DATE_ORDER)) ? true : false;
+	
 	}
 
 	private String removeBy(String userTask) {
-		String[] str = userTask.toLowerCase().split(STRING_SPLITTER);
-		if (str[0].contains(PREPOSITION_BY)) {
+		
+		assert(userTask != null);
+		logger.log(Level.INFO, LOGGER_MESSAGE_REMOVE_BY);
+		
+		stringSplit = userTask.toLowerCase().split(STRING_SPLITTER);
+		
+		if (stringSplit[POSITION_OF_INPUT].contains(PREPOSITION_BY)) {
 			userTask = userTask.replace(PREPOSITION_BY, STRING_EMPTY);
 		}
+		
 		return userTask.trim();
 	}
 }
